c22cf73e4009224e4b2e24ed194fa309d70b0971
EmersonCcp <emerson.paixao52@gmail.com>
2024-10-08 08:11:48 -0300

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test';
+  
+  colums = ['Nombre','Apellido','Edad'];
+
+  keys = ['cli_nombre','cli_apellido','cli_edad'];
+
+  clientes:any[] = [
+    {
+      cli_nombre: 'Emerson',
+      cli_apellido: 'Nunez',
+      cli_edad: 24
+    },
+    {
+      cli_nombre: 'Saida',
+      cli_apellido: 'Pereira',
+      cli_edad: 64
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './components/title/title.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    ButtonComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
